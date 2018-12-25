f4dfd3a4eebb9ce1ba039c979c866908ba8f33e1
wangding <408542507@qq.com>
2018-12-25 18:04:07 +0800

    change

--- 04-process/01-information.js
+++ 04-process/01-information.js
@@ -11,12 +11,12 @@ log('exePath:%s\n',process.execPath);
 log('node version:',process.version);
 log('user id:',process.getuid());
 log('group id',process.getgid());
-log('cwd:%d\n',process.cwd());
+log('cwd:',process.cwd());
 
 log('rss:',process.memoryUsage().rss);
 log('heapTotal:',process.memoryUsage().heapTotal);
-log('heapUsed:',process.memoryUsage.heapUsed);
-log('external:%s\n',process.memoryUsage.external);
+log('heapUsed:',process.memoryUsage().heapUsed);
+log('external:%s\n',process.memoryUsage().external);
 
-log('env:',process.env);
+//log('env:',process.env);
 log('host name:',process.env.HOSTNAME);
