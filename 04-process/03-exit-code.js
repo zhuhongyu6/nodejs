0fafba99345d0579c4a009bc749e72073a7ac077
zhuhongyu <1315211512@qq.com>
2018-12-26 16:55:39 +0800

    change

--- 04-process/03-exit-code.js
+++ 04-process/03-exit-code.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 var code=process.argv[2];
-console.log(code);
+//console.log(code);
 if(process.argv.length<3){
 console.log('请给出命令行参数！');
 process.exit(1);
 }
 
-if(typeof code!=='number'){
+if(isNaN(Number(code))){
   console.error('命令行参数类型应该是数值类型');
   process.exit(1);
 }
