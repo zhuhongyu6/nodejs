3dcb41d3c666bdb1d793a703068076266ec663fb
zhuhongyu <1315211512@qq.com>
2018-12-28 10:12:57 +0800

    change

--- 05-timer/01-delay-call.js
+++ 05-timer/01-delay-call.js
@@ -5,11 +5,10 @@ function Bomb(){
 }
 
 Bomb.prototype.explode=function(){
-  //这里的this指setTimeout而不是实例化出来的对象,所以后面用bind方法改变指向
   console.log(this);
   console.log(this.message);
 }
 
 var b=new Bomb();
-
+//通过bind方法确保方法绑定到正确的对象上。绑定确保该方法内部的代码可以访问对象的内部属性。否则，setTimeout会导致this绑定到全局对象上运行。
 setTimeout(b.explode.bind(b),2000);
