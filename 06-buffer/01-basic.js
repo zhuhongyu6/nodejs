7f31f9eac4b6c10d5abe8604d5ddc9386a51fcb7
zhuhongyu <1315211512@qq.com>
2018-12-29 09:58:57 +0800

    change

--- 06-buffer/01-basic.js
+++ 06-buffer/01-basic.js
@@ -3,19 +3,34 @@
 var buf1=new Buffer(256);
 buf1[0]=23;
 
-const log=console.log;
-log('buffer length:',buf1.length);
-log('\nbuffer content:',buf1);
+console.log('buf1 length:',buf1.length);
+console.log('\nbuf1 content:',buf1);
 
 for(var i=0;i<256;i++){
   buf1[i]=i;
 }
-log('\nbuffer content:',buf1);
+console.log('\nbuf1 content:',buf1);
 
 //buffer切片操作
 var buf2=buf1.slice(250,256);
-log('\nbuffer content:',buf2);
+console.log('\nbuf2 content:',buf2);
 
 //用0填充buf2
 buf2.fill(0);
-log('\nbuffer content:',buf2);
+console.log('\nbuf2 content:',buf2);
+
+//用数组初始化buf3
+var array=['a',0xba,0xdf,0x00,255,10];
+var buf3=new Buffer(array);
+console.log('\nbuf3:',buf3.length,buf3);
+
+//用字符串初始化buf4
+var buf4=new Buffer('hello','utf8');
+console.log('\nbuf4:',buf4.length,buf4.toString());
+
+//将buf4的内容复制到buf5中
+var buf5=new Buffer(buf4.length);
+buf4.copy(buf5,0,0,buf4.length);
+console.log('\nbuf5:',buf5.length,buf5.toString());
+
+
