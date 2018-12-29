6cfc9b853ce6862f2a834324e625075f4a3f6225
zhuhongyu <1315211512@qq.com>
2018-12-29 10:37:52 +0800

    change

--- 06-buffer/02-encode.js
+++ 06-buffer/02-encode.js
@@ -4,5 +4,11 @@ const name=process.argv[2];
       pwd=process.argv[3];
 
 var str =name+":"+pwd;
+//console.log(str);
+var buf=new Buffer(str);
+
+console.log('user name and passwd:',buf.toString());
+console.log('user name and passwd with base64:',buf.toString('base64'));
+console.log('user name and passwd with hex:',buf.toString('hex'));
 
 
